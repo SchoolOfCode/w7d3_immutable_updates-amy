992f41c374da02767525dbc4e64db1452c0e4b3b
AmandaRichards <Richards.amanda92@gmail.com>
2022-01-06 13:59:34 +0000

    replace item complete

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ export function insertItem(array, item, index) {
 }
 
 // Immutably replace an item at a specific position/index within an array
-export function replaceItem(array, item, index) {}
+export function replaceItem(array, item, index) {
+    return [...array.slice(0, index), item, ...array.slice(index + 1)]
+}
 
 // Immutably remove an item at a specific position/index within an array
 export function removeItem(array, index) {}
